ec8ab72ff0108b59ba869eb38c95e3f98a9763e3
Sergey73LSA <73otverstie@gmail.com>
2023-01-01 17:23:43 +0400

    Database connection file

---
+++ db_connect.php
@@ -0,0 +1,33 @@
+<?php
+
+$mysqli = mysqli_connect($servername, $username, $db_password, $db);
+if (!$mysqli) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
