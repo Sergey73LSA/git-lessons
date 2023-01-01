dfb527cb70a5ba29bda41f5d2cddd516318d5b48
Sergey73LSA <73otverstie@gmail.com>
2023-01-01 17:21:44 +0400

    Variables for working form

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Подключение к базе
+	$servername = "localhost";
+	$username = "root";
+	$db_password = "1234qwer";
+	$db = "connect_with_us";
+
+    // Отправка уведомлений на почту
+    $host = 'ssl://smtp.yandex.ru'; // SMTP сервера вашей почты
+    $port = 465;
+    $mymail = 'otverstie73@yandex.ru'; // Логин на почте
+    $pass = 'K9f7H5w3T1d'; // Пароль на почте
+    $sendername = 'simtechTest'; // Имя отправителя
+    $mymail2 = 'sergey73_91@mail.ru'; // Почта куда придет письмо
+
+?>
